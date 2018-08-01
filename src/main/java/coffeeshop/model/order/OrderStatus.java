9e3bce5338a736673871c68ce304a4eb1f04cb92
Nguyen Dinh Duc <duc2495@github.com>
2018-08-01 11:45:44 +0700

    create order status page

--- src/main/java/coffeeshop/model/order/OrderStatus.java
+++ src/main/java/coffeeshop/model/order/OrderStatus.java
@@ -1,5 +1,18 @@
 package coffeeshop.model.order;
 
 public enum OrderStatus {
-	ORDERED, SHIPPING, FINISHED, CANCELED
+	ORDERED(1), SHIPPING(2), FINISHED(3), CANCELED(0);
+	private int value;
+
+	OrderStatus(int value) {
+		this.setValue(value);
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
 }
